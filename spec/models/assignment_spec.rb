ec40f2140726c1b66aed083c02c37ef629f241a6
Cath Komrij <komcath@gmail.com>
2024-03-26 16:27:58 -0700

    added calling service from assignment model

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -3,4 +3,58 @@ RSpec.describe Assignment, type: :model do
     it { is_expected.to belong_to :ride }
     it { is_expected.to belong_to :driver }
   end
+
+  describe "Callbacks" do
+    describe ".set_attributes after_save" do
+      let(:driver) { create(:driver) }
+      let(:ride) { create(:ride, ride_duration: rand(1.1..3.5), ride_earnings: rand(12.0..25.0))}
+
+      shared_examples "triggers set_attributes after_save" do
+        it "triggers" do
+          assignment = create(:assignment, ride:ride, driver: driver,
+                              commute_duration: commute_duration, score: score)
+          allow(assignment).to receive(:set_attributes)
+          assignment.save
+          expect(assignment).to have_received(:set_attributes)
+        end
+      end
+
+      context "when commute_duration is nil" do
+        let(:commute_duration) { nil }
+        let(:score) { rand(10.0..30.0) }
+        include_examples "triggers set_attributes after_save"
+      end
+
+      context "when score is nil" do
+        let(:commute_duration) { rand(1.1..3.5) }
+        let(:score) { nil }
+        include_examples "triggers set_attributes after_save"
+      end
+
+      context "when necessary attributes are NOT nil" do
+        let(:commute_duration) { rand(1.1..3.5) }
+        let(:score) { rand(10.0..30.0) }
+
+        it "does not trigger set_attributes" do
+          assignment = create(:assignment, ride:ride, driver: driver,
+                              commute_duration: commute_duration, score: score)
+          allow(assignment).to receive(:set_attributes)
+          assignment.save
+          expect(assignment).not_to have_received(:set_attributes)
+        end
+      end
+
+      context "when necessary references are NOT present" do
+        let(:commute_duration) { nil }
+        let(:score) { nil }
+
+        it "does not trigger set_attributes" do
+          assignment = create(:assignment, commute_duration: commute_duration, score: score)
+          allow(assignment).to receive(:set_attributes)
+          assignment.save
+          expect(assignment).not_to have_received(:set_attributes)
+        end
+      end
+    end
+  end
 end
