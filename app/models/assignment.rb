f29be5f82cd3e7be1596c2cf5e61c7aee1b4aaac
Cath Komrij <komcath@gmail.com>
2024-03-26 14:23:01 -0700

    add annotate gem

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: assignments
+#
+#  id               :bigint           not null, primary key
+#  commute_duration :float
+#  score            :float
+#  driver_id        :bigint           not null
+#  ride_id          :bigint           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+
 class Assignment < ApplicationRecord
   belongs_to :driver
   belongs_to :ride
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: drivers
+#
+#  id           :bigint           not null, primary key
+#  home_address :json
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+
 class Driver < ApplicationRecord
   has_many :assignments
   has_many :rides, through: :assignments
