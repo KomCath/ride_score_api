15e844ecb15a1ef6bf0777fadba1ba58ab6230ab
Cath Komrij <komcath@gmail.com>
2024-03-27 22:08:33 -0700

    added routes and controller assignments_controller

---
+++ app/controllers/v1/assignments_controller.rb
@@ -0,0 +1,60 @@
+module V1
+  class AssignmentsController < ApplicationController
+    before_action :find_driver, only: [:index]
+
+    def index
+      paginated_assignments = sorted_assignments.page(params[:page]).per(DEFAULT_PER_PAGE_SIZE)
+      render json: paginated_assignments.map { |assignment| serialize_assignment(assignment) }
+    end
+
+    private
+
+    DEFAULT_PER_PAGE_SIZE = 10
+
+    def find_driver
+      @driver = Driver.find_by(id: params[:driver_id])
+
+      render json: { error: "Driver not found" }, status: :not_found unless @driver
+    end
+
+    def sorted_assignments
+      return [ ] unless @driver
+
+      @driver.assignments.where.not(score: nil).includes(:ride).order(score: :desc)
+    end
+
+    def serialize_assignment(assignment)
+      {
+        score: assignment.score,
+        # start_address: assignment.ride.start_address,
+        # destination_address: assignment.ride.destination_address,
+        commute_duration: format_duration(assignment.commute_duration),
+        ride_duration: format_duration(assignment.ride.ride_duration),
+        ride_earnings: "$#{assignment.ride.ride_earnings}"
+      }
+    end
+
+    # def format_address(address)
+      
+    # end
+
+    def format_duration(duration)
+      total_minutes = (duration * 60).to_i
+      hours = total_minutes / 60
+      minutes = total_minutes % 60
+
+      hour_string = hours == 1 ? "hour" : "hours"
+      minute_string = minutes == 1 ? "minute" : "minutes"
+
+      if hours.positive? && minutes.positive?
+        "#{hours} #{hour_string} and #{minutes} #{minute_string}"
+      elsif hours.positive?
+        "#{hours} #{hour_string}"
+      elsif minutes.positive?
+        "#{minutes} #{minute_string}"
+      else
+        "0 minutes"
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :v1 do
+    scope ":driver_id" do
+      resources :assignments, only: [:index]
+    end
+  end
 end
