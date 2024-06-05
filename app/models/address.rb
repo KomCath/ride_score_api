9c20a0a53708281801d166c87a3521e11c15cecd
Cath Komrij <komcath@gmail.com>
2024-06-05 12:45:21 -0700

    create migration and model for address

---
+++ app/models/address.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id         :bigint           not null, primary key
+#  line1      :string           not null
+#  line2      :string
+#  city       :string           not null
+#  state      :string           not null
+#  zip_code   :string           not null
+#  country    :string           default("US")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Address < ApplicationRecord
+end
