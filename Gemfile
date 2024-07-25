f7af1d046a8632f5f8416bcd725eaf4856bd4fff
Cath Komrij <komcath@gmail.com>
2024-07-25 14:50:02 -0700

    added state machine to address model

--- Gemfile
+++ Gemfile
@@ -3,14 +3,16 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
-# Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.8", ">= 7.0.8.1"
-
-# Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
-
-# Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
+
+gem "aasm"
+gem "bootsnap", require: false
+gem "geocoder"
+gem "httparty"
+gem "kaminari"
+gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
@@ -24,31 +26,21 @@ gem "puma", "~> 5.0"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
-
-# Reduces boot times through caching; required in config/boot.rb
-gem "bootsnap", require: false
-
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem "rack-cors"
 
-gem "httparty"
-gem "kaminari"
-gem "geocoder"
-
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
-  gem "rspec-rails"
+  gem "dotenv-rails"
   gem "factory_bot_rails"
   gem "faker"
-  gem "shoulda-matchers", "~> 6.0"
-  gem "dotenv-rails"
   gem "pry-byebug"
+  gem "rspec-rails"
+  gem "shoulda-matchers", "~> 6.0"
   gem "vcr"
 end
 
--- app/models/address.rb
+++ app/models/address.rb
@@ -2,18 +2,50 @@
 #
 # Table name: addresses
 #
-#  id         :bigint           not null, primary key
-#  line1      :string           not null
-#  line2      :string
-#  city       :string           not null
-#  state      :string           not null
-#  zip_code   :string           not null
-#  country    :string           default("US")
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                  :bigint           not null, primary key
+#  line1               :string           not null
+#  line2               :string
+#  city                :string           not null
+#  state               :string           not null
+#  zip_code            :string           not null
+#  country             :string           default("US")
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  verification_status :string
 #
 class Address < ApplicationRecord
+  include AASM
+
   has_one :verified_address, dependent: :destroy
 
   validates_presence_of :line1, :city, :state, :zip_code
+
+  aasm column: :verification_status do
+    # default verification_status for `new` records
+    state :verification_not_attempted, initial: true
+
+    # the address verification was attempted but failed for a system reason (could not connect?)
+    state :unable_to_perform_verification
+
+    # the address was not verified and the data needs to be confirmed
+    state :verification_pending
+
+    # the address has been verified successfully
+    state :verification_successful
+
+    event :mark_unable_to_perform_verification do
+      transitions from: %i[verification_not_attempted verification_pending],
+        to: :unable_to_perform_verification
+    end
+
+    event :mark_verification_pending do
+      transitions from: %i[verification_not_attempted unable_to_perform_verification],
+        to: :verification_pending
+    end
+
+    event :mark_verification_successful do
+      transitions from: %i[verification_not_attempted unable_to_perform_verification verification_pending],
+        to: :verification_successful
+    end
+  end
 end
