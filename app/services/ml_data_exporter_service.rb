8219138572ab7b9f872ff88996345b37eee5f194
Cath Komrij <komcath@gmail.com>
2025-06-03 13:47:49 -0700

    Add ml_data_exporter_service and specs

--- app/services/ml_data_exporter_service.rb
+++ app/services/ml_data_exporter_service.rb
@@ -1,13 +1,40 @@
-require "csv"
-
 class MlDataExporterService
-  EXPORT_PATH = Rails.root.join("ml_models", "ride_data.csv")
+  EXPORT_PATH = Rails.root.join("ml_models", "assignment_data.csv")
+  CSV_HEADERS = %w[ride_duration ride_earnings commute_duration score].freeze
 
   def self.export
-    CSV.open(EXPORT_PATH, "w") do |csv|
-      csv << ["ride_duration", "ride_earnings", "commute_duration", "score"]
+    new.export
+  end
 
-      Assignment.includes(:ride).find_each do |assignment|
+  def export
+    begin
+      tempfile = Tempfile.new(["ml_assignment_data", ".csv"])
+
+      raise "No assignments found to export." if assignments.empty?
+
+      write_csv(tempfile, assignments)
+      FileUtils.mv(tempfile.path, EXPORT_PATH)
+
+      Rails.logger.info("MlDataExporterService.export Success")
+    rescue StandardError => e
+      Rails.logger.error("MlDataExporterService Unexpected error: #{e.message}")
+    ensure
+      tempfile.close
+      tempfile.unlink
+    end
+  end
+
+  private
+
+  def assignments
+    @assignments ||= Assignment.includes(:ride)
+  end
+
+  def write_csv(file, assignments)
+    CSV.open(file.path, "w") do |csv|
+      csv << CSV_HEADERS
+
+      assignments.each do |assignment|
         csv << [
           assignment.ride.ride_duration,
           assignment.ride.ride_earnings,
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'aasm/rspec'
+require 'csv'
 require 'rspec/rails'
 require 'spec_helper'
 
