7ca79b1a4428a62207d1d45f0f85e22a450addb2
Cath Komrij <komcath@gmail.com>
2025-06-30 11:28:21 -0700

    add gem, fix path, fix syntax

--- app/services/score_prediction_service.rb
+++ app/services/score_prediction_service.rb
@@ -2,7 +2,7 @@ require "pycall/import"
 include PyCall::Import
 
 class ScorePredictionService
-  MODEL_PATH = Rails.root.join("ml_models", "score_predictor.pkl").to_s
+  MODEL_PATH = Rails.root.join("ml_models", "assignment_model.pkl").to_s
 
   def initialize(ride_duration, ride_earnings, commute_duration)
     @ride_duration = ride_duration
@@ -18,6 +18,6 @@ class ScorePredictionService
     input_data = np.array([[@ride_duration, @ride_earnings, @commute_duration]])
     prediction = model.predict(input_data)
 
-    prediction.first.round(2)
+    prediction[0].round(2).to_f
   end
 end
